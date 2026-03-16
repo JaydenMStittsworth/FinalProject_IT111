71bc81eec829d56f21523c600960bf9cb2137353
Jayden <Jayden@DESKTOP-50330FE>
2026-03-15 20:34:34 -0700

    Created the Records list that stores elements of the Record class

--- FinalProject_IT111/frmDocumentationApp.cs
+++ FinalProject_IT111/frmDocumentationApp.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
+
 namespace FinalProject_IT111
 {
     public partial class frmDocumentationApp : Form
     {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public List<Record> Records { get; set; }
         public frmDocumentationApp()
         {
             InitializeComponent();
+            Records = new List<Record>();
         }
     }
 }
